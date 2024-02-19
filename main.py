8f263126976eede71fd661634ffaf9decb8c48e4
Edmond <edmond@SYFL-MLPT-147.local>
2024-02-19 10:58:00 +0000

    Chatbot application with ConversationBufferMemory

--- main.py
+++ main.py
@@ -1,51 +1,28 @@
-from langchain_openai import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
-import argparse
 
 load_dotenv()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="return a list of numbers")
-parser.add_argument("--language", default="python")
-args = parser.parse_args()
-
-llm = OpenAI()
-
-code_prompt = PromptTemplate(
-    input_variables=["task", "language"],
-    template="Write a very short {language} function that will {task}."
-)
-test_prompt = PromptTemplate(
-    input_variables=["language", "code"],
-  template="Write a unit test with the leading testing framework syntax for {language} for this code:\n{code}"
+chat = ChatOpenAI()
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt,
-    output_key="code"
-)
-test_chain = LLMChain(
-    llm=llm,
-    prompt=test_prompt,
-    output_key="test"
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory
 )
 
-chain = SequentialChain(
-    chains=[code_chain, test_chain],
-    input_variables=["task", "language"],
-    output_variables=["test", "code"]
-)
-
-result = chain.invoke({
-    "language": args.language,
-    "task": args.task
-})
-
-print(">>>>>> GENERATED CODE:")
-print(result["code"])
-
-print(">>>>>> GENERATED TEST:")
-print(result["test"])
+while True:
+    content = input(">> ")
+    result = chain.invoke({"content": content})
+    print(result["text"])
